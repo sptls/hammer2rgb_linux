8b265cf3fbf34c681503bbc95deea45b6b33ec41
sptls <sp0tlesss@yahoo.com>
2023-06-21 17:37:37 +0200

    Updated version script

--- update_version.sh
+++ update_version.sh
@@ -3,6 +3,7 @@
 VERSION=`grep -e "#define HAMMER2RGB_VERSION" src/constants.h | grep -o -E "[0-9]{1,3}\.[0-9]{1,5}"`
 echo $VERSION > VERSION
 sed -Ei "s/^AC_INIT\(\[hammer2rgb\].*\)/AC_INIT\(\[hammer2rgb\], \[${VERSION}\]\)/" configure.ac
+sed -Ei "s/^AC_INIT\(\[hammer2rgb\].*\)/AC_INIT\(\[hammer2rgb\], \[${VERSION}\]\)/" src/configure.ac
 sed -Ei "s/^pkgver=.*/pkgver=${VERSION}/" hammer2rgb-aur/PKGBUILD
 
 CUR_DIR=$PWD
